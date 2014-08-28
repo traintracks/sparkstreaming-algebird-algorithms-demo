11fe8e64f77a465d5055e6378c5016694a8b7871
boson <boson@traintracks.io>
2014-08-28 22:08:01 +0800

    fix the no output result problem & make algebirdexample sing

--- src/main/scala/io/traintracks/demo/spark/streaming/algebird/AlgebirdExampleDemo.scala
+++ src/main/scala/io/traintracks/demo/spark/streaming/algebird/AlgebirdExampleDemo.scala
@@ -3,30 +3,33 @@ package io.traintracks.demo.spark.streaming.algebird
 /**
  * Created by boson on 8/28/14.
  */
-import com.twitter.algebird._
-import com.twitter.algebird.Operators._
-import org.apache.spark.streaming.StreamingContext._
-import org.apache.spark.api.java.function._
-import org.apache.spark.streaming._
 
+import com.twitter.algebird.Max
+import org.apache.spark.streaming._
+import org.apache.log4j.{Level, Logger}
+import com.twitter.algebird.Operators._
 
 object AlgebirdExampleDemo {
-  def main(args: Array[String]): Unit = {
 
-    val master = "local"
+  def getMap(line: String): Map[Int, Max[Int]] = {
+    val arr = line.split(" ")
+    if (arr.length == 2) {
+      return Map( arr(0).toInt -> Max(arr(1).toInt))
+    }
+    return Map()
+  }
+
+  def main(args: Array[String]) {
+    val log4jInitialized = Logger.getRootLogger.getAllAppenders.hasMoreElements
+    if (!log4jInitialized) {
+      Logger.getRootLogger.setLevel(Level.WARN)
+    }
+    val master = "local[2]"
     // Create a StreamingContext with a local master
-    val ssc = new StreamingContext(master, "AlgebirdExampleDemo", Seconds(1))
+    val ssc = new StreamingContext(master, "AlgebirdExampleDemo", Seconds(4))
     // Create a Dstream that will connect to serverIP:serverPort, like localhost:9999
-    var result = Map(1 -> Max(2))
     val lines = ssc.socketTextStream("localhost", 9999)
-    val words = lines.map(line => {
-      val numbers = line.split(" ")
-      if (numbers.length == 2) {
-        line.map(Map(numbers(0).toInt -> Max(numbers(1).toInt)))
-        println(result)
-      }
-    }).reduce(_ + _)
-    words.print()
+    lines.map(line => getMap(line)).reduce(_ + _).print()
     ssc.start()
     ssc.awaitTermination()
   }
