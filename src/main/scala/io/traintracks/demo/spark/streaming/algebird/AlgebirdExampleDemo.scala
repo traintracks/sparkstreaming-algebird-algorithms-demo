37da109805b0c5ffb338c656e94ac524372b0057
boson <boson@traintracks.io>
2014-09-02 12:50:47 +0800

    format code

--- src/main/scala/io/traintracks/demo/spark/streaming/algebird/AlgebirdExampleDemo.scala
+++ src/main/scala/io/traintracks/demo/spark/streaming/algebird/AlgebirdExampleDemo.scala
@@ -8,20 +8,21 @@ import com.twitter.algebird.Max
 import org.apache.spark.streaming._
 import com.twitter.algebird.Operators._
 
-object AlgebirdExampleDemo extends App{
+object AlgebirdExampleDemo extends App {
   def getMap(line: String): Map[Int, Max[Int]] = {
     val arr = line.split(" ")
     if (arr.length == 2) {
-      return Map( arr(0).toInt -> Max(arr(1).toInt))
+      return Map(arr(0).toInt -> Max(arr(1).toInt))
     }
     return Map()
   }
-    val master = "local[2]"
-    // Create a StreamingContext with a local master
-    val ssc = new StreamingContext(master, "AlgebirdExampleDemo", Seconds(4))
-    // Create a Dstream that will connect to serverIP:serverPort, like localhost:9999
-    val lines = ssc.socketTextStream("localhost", 9999)
-    lines.map(line => getMap(line)).reduce(_ + _).print()
-    ssc.start()
-    ssc.awaitTermination()
+
+  val master = "local[2]"
+  // Create a StreamingContext with a local master
+  val ssc = new StreamingContext(master, "AlgebirdExampleDemo", Seconds(4))
+  // Create a Dstream that will connect to serverIP:serverPort, like localhost:9999
+  val lines = ssc.socketTextStream("localhost", 9999)
+  lines.map(line => getMap(line)).reduce(_ + _).print()
+  ssc.start()
+  ssc.awaitTermination()
 }
